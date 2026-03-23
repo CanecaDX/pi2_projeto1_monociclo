4c1c5134de55af6f9edc1be28d422d7fdfd88040
Sara Vitoria henssler <saravitoriahenssler@gmail.com>
2026-03-23 12:45:53 -0300

    Finish decoder function
    registers bank done, finish decoder function

--- mars_main.c
+++ mars_main.c
@@ -1,12 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 
-#define MEM_SIZE 256
-
+//constantes
+#define INST_MEM_SIZE 256
+//tipo r
+struct inst_r{
+		uint8_t opcode;
+		uint8_t rs;
+		uint8_t rt;
+		uint8_t rd;
+		uint8_t function;
+	};
+//tipo i
+struct inst_i{
+		uint8_t opcode;
+		uint8_t rs;
+		uint8_t rt;
+		uint8_t imm;
+	};
+//tipo j
+struct inst_j{
+		uint8_t opcode;
+		uint8_t addres;
+	};
+	
+//funções
 uint16_t *get_mem_file(char *mem_name, int *size);
+uint16_t *init_regs();
+//recebe PC como parâmetro
+uint8_t  *decoder(uint16_t *program_mem, int *PC);
+
 
 int main(){
 	int op;
+	uint16_t *regs = init_regs();
+	uint16_t *program_mem = NULL;
+	uint16_t *data_mem = NULL;
 	
 	do{
 		printf("\n------------------------------------");
@@ -67,3 +97,32 @@ int main(){
 	}while(op != 0);
 	return 0;
 }
+
+uint16_t *init_regs(){
+	uint16_t *regs = calloc(8, sizeof(uint16_t));
+	return regs;
+}
+	/*
+	 * uint8_t  *decoder(uint16_t *program_mem, int *PC){
+	uint16_t inst_mem = program_mem[PC];
+	
+	//pega os 4 MSB de inst
+	uint8_t opcode = (inst_mem >> 12) & 0xF; 
+	
+	if(opcode = 0){
+		struct inst_r *inst = NULL;
+		inst.opcode   = (inst >> 12) & 0xF;  
+		inst.rs       = (inst >> 9)  & 0x7;  
+		inst.rt       = (inst >> 6)  & 0x7;  
+		inst.rd       = (inst >> 3)  & 0x7; 
+		inst.function =  inst        & 0x7;  
+		return inst;
+	}else if(opcode = 0100){
+		struct inst_i *inst = NULL;
+		
+		
+	}
+}*/
+
+
+
