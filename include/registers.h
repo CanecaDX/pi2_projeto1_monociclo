4027f6aec281aab2a0615639ad18563983c319f2
Sara Vitoria henssler <saravitoriahenssler@gmail.com>
2026-03-29 19:48:52 -0300

    Delete include directory

--- include/registers.h
+++
@@ -1,9 +0,0 @@
-#ifndef REGISTERS_H
-#define REGISTERS_H
-
-#include <stdint.h>
-
-uint16_t *init_regs(void);
-void print_regs(uint16_t *regs, int count);
-
-#endif
--- include/run.h
+++
@@ -1,8 +0,0 @@
-#ifndef RUN_H
-#define RUN_H
-
-#include <stdint.h>
-
-void executar_programa(uint16_t *program_mem, int program_mem_size);
-
-#endif
